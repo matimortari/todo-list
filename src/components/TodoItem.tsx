16d5d29fd2417f87b08e5e5f9343a35e30987cd1
matheus <matheus.felipe.19rt@gmail.com>
2024-07-24 23:51:31 -0300

    style: Update TodoItem component styles

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,11 +2,11 @@ export default function TodoItem() {
 	return (
 		<li className="button flex max-w-4xl">
 			<div className="flex items-center gap-2">
-				<input type="checkbox" className="h-6 w-6 rounded" />
+				<input type="checkbox" className="checkbox h-6 w-6" />
 				<p className="text-lg">Todo</p>
 			</div>
 
-			<button className="">X</button>
+			<button className="button-delete ml-auto">X</button>
 		</li>
 	)
 }
